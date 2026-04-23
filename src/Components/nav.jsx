c5655f53d853b8248f0bba952e826f72943f60e1
Taqi Saud <taqifreeancer1367@gmail.com>
2026-04-23 19:13:51 +0500

    react

--- src/Components/nav.jsx
+++ src/Components/nav.jsx
@@ -2,12 +2,12 @@ function Nav(){
     return(
         <div className="flex justify-between items-center bg-gray-600 p-4">
             <img src="#" alt="Logo" />
-            <nav className="text-white">
+            <nav className="text-white flex gap-4">
                 <a href="#">Home</a>
                 <a href="#">Contact US</a>
                 <a href="#">About US</a>
             </nav>
-            <button>Click Here</button>
+            <button className="bg-blue-600 text-cyan-50 p-2 rounded-2xl">Click Here</button>
         </div>
     )
 }
