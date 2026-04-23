0ffbf4cdc911207b178cffe4e012dc8942de7f0a
Taqi Saud <taqifreeancer1367@gmail.com>
2026-04-23 18:31:08 +0500

    react

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import Cards from './Components/Cards'
 import d_style from './Components/diff_style'
 import './App.css'
-const cards=[{id="1",title="Web Dev",p="3Months",btn="Click Here",style="bg-red-300"},
-  {id="2",title="SEO",p="2 Months",btn="Explore More",style="bg-blue-300"},
-  {id="3",title="CEH",p="1 Months",btn="Click Here",style="bg-grey-300"}
+const cards=[{id:"1",title:"Web Dev",p:"3Months",btn:"Click Here",style:"bg-red-300"},
+  {id:"2",title:"SEO",p:"2 Months",btn:"Explore More",style:"bg-blue-300"},
+  {id:"3",title:"CEH",p:"1 Months",btn:"Click Here",style:"bg-grey-300"}
 ]
 function App() {
   return (
