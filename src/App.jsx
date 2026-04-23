abf30117032a302c2d536e9ecdd708bcef04b7dd
Taqi Saud <taqifreeancer1367@gmail.com>
2026-04-23 18:33:35 +0500

    react

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
 import Cards from './Components/Cards'
 import d_style from './Components/diff_style'
 import './App.css'
-const cards=[{id:"1",title:"Web Dev",p:"3Months",btn:"Click Here",style:"bg-red-300"},
-  {id:"2",title:"SEO",p:"2 Months",btn:"Explore More",style:"bg-blue-300"},
-  {id:"3",title:"CEH",p:"1 Months",btn:"Click Here",style:"bg-grey-300"}
+
+const cards = [
+  { id: "1", title: "Web Dev",  p: "3 Months", btn: "Click Here",   style: "bg-red-300"  },
+  { id: "2", title: "SEO",      p: "2 Months", btn: "Explore More", style: "bg-blue-300" },
+  { id: "3", title: "CEH",      p: "1 Month",  btn: "Click Here",   style: "bg-gray-300" },
 ]
+
 function App() {
   return (
     <>
-    {cards.map((card)=>{
-      <Cards key={card.id} title={card.title} p={card.p} btn={card.btn}/>
-    })}
-    <div>
-      {cards.map((card)=>{
-      <d_stylee key={card.id} title={card.title} p={card.p} btn={card.btn}/>
-    })}
-    </div>
+      {cards.map((card) => (
+        <Cards key={card.id} title={card.title} p={card.p} btn={card.btn} />
+      ))}
+
+      <div>
+        {cards.map((card) => (
+          <d_style key={card.id} title={card.title} p={card.p} btn={card.btn} />
+        ))}
+      </div>
     </>
   )
 }
