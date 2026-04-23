3b8e4be6cb04ea86a60d48b06c8024d923c4e56d
Taqi Saud <taqifreeancer1367@gmail.com>
2026-04-23 18:16:04 +0500

    react

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import Cards from './Components/Cards'
 import './App.css'
-
+const cards=[{id="1",title="Web Dev",p="3Months",btn="Click Here"},
+  {id="2",title="SEO",p="2 Months",btn="Explore More"},
+  {id="3",title="CEH",p="1 Months",btn="Click Here"}
+]
 function App() {
-
-
   return (
     <>
-      
+    {cards.map((card)=>{
+      <Cards key={cards.id} title={cards.title} p={cards.p} btn={cards.btn}/>
+    })}
     </>
   )
 }
