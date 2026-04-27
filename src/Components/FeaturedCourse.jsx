3a47d8154cc7853b3defb36d480d7173ad452ed2
Taqi Saud <taqifreeancer1367@gmail.com>
2026-04-27 18:28:46 +0500

    react

--- src/Components/FeaturedCourse.jsx
+++ src/Components/FeaturedCourse.jsx
@@ -1,9 +1,16 @@
+import { LucideCloudLightning } from "lucide-react"
+const imgs=[{id:"1",img:"<LucideCloudLightning/>"},
+    {id:"2",img:"#"},
+    {id:"3",img:"$"}
+]
 function FeaturedCourse(){
     return(
         <div>
-            <div className="flex">
-                <h1>Featured Course</h1>
-                <button>Click Here</button>
+            <div className="mt-15 flex flex-col justify-center items-center">
+                <h1 className="text-2xl">Featured Course</h1>
+                <span> imgs.map((img)=>{
+                    })</span>
+                <button className="text-xl">Click Here</button>
             </div>
         </div>
     )
--- src/Components/routes/Home.jsx
+++ src/Components/routes/Home.jsx
@@ -1,9 +1,20 @@
+import FeaturedCourse from "../FeaturedCourse"
 import Hero from "../Hero"
 function Home(){
     return(
-        <section className="mt-10 shadow-cyan-950 shadow-lg rounded-2xl p-3  gap-3">
+         <div>
+                    <section className="mt-10 shadow-cyan-950 shadow-lg rounded-2xl p-3  gap-3">
             <Hero/>
+           
         </section>
+        <div className="flex justify-between items-center p-4">
+                 <FeaturedCourse/>
+                 <FeaturedCourse/>
+                 <FeaturedCourse/>
+
+        </div>
+         </div>
+
     )
 }
 export default Home
